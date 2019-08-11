191e0a92fd30fc1359b67e1a07054134d561c6be
ichingking <ichingking@yandex.com>
2019-08-11 15:26:42 +0800

    add auto check ocaml files

--- k-ocaml.el
+++ k-ocaml.el
@@ -2,11 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(push "/home/*****/.opam/4.08/share/emacs/site-lisp/" load-path)
-;; (add-to-list 'load-path "/home/*****/.opam/4.08/share/emacs/site-lisp/")
-(autoload 'merlin-mode "merlin" "Merlin mode" t)
+(push "/home/ic_king/.opam/4.08/share/emacs/site-lisp/" load-path)
+;; (add-to-list 'load-path "/home/ic_king/.opam/4.08/share/emacs/site-lisp/")
 (autoload 'tuareg-mode "tuareg" "Tuareg mode" t)
+(load "tuareg-site-file")
+(autoload 'merlin-mode "merlin" "Merlin mode" t)
 (add-hook 'tuareg-mode-hook 'merlin-mode)
 (add-hook 'caml-mode-hook 'merlin-mode)
+
+(add-to-list 'auto-mode-alist '("\\.\\(ml\\|mli\\)\\'" . merlin-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(ml\\|mli\\)\\'" . tuareg-mode))
+
 (provide 'k-ocaml)
 ;;; k-ocaml.el ends here
