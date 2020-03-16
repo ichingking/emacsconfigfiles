d605166545aac3ad3a25f4380ba8bfd72fb62fba
ichingking <ichingking@yandex.com>
2020-03-17 00:43:02 +0800

    changed

--- k-ocaml.el
+++ k-ocaml.el
@@ -2,16 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(push "/home/ic_king/.opam/4.08/share/emacs/site-lisp/" load-path)
-;; (add-to-list 'load-path "/home/ic_king/.opam/4.08/share/emacs/site-lisp/")
+
+(load "/home/ic_king/.opam/default/share/emacs/site-lisp/tuareg-site-file")
+(load "/home/ic_king/.opam/default/share/emacs/site-lisp/tuareg-menhir")
+
 (autoload 'tuareg-mode "tuareg" "Tuareg mode" t)
 (load "tuareg-site-file")
 (autoload 'merlin-mode "merlin" "Merlin mode" t)
+
 (add-hook 'tuareg-mode-hook 'merlin-mode)
 (add-hook 'caml-mode-hook 'merlin-mode)
 
 (add-to-list 'auto-mode-alist '("\\.\\(ml\\|mli\\)\\'" . merlin-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(ml\\|mli\\)\\'" . tuareg-mode))
+
+(setq merlin-command 'opam)
 
 (provide 'k-ocaml)
 ;;; k-ocaml.el ends here
